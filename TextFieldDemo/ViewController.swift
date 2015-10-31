f90f0bf0bc8cbebc3b98af305be2283f467dd9ab
Boyko Andrey <_flanker_4@mail.ru>
2015-10-31 13:53:21 +0200

    demo for answer

--- TextFieldDemo/ViewController.swift
+++ TextFieldDemo/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
 
+        if (string.characters.count==0){
+            return true
+        }
+        
+        //Получаем текущий текст textField'a
+        let text = ((textField.text ?? "") as NSString)
+        //Получаем текст textField'a после редактирования
+        let newText = text.stringByReplacingCharactersInRange(range, withString: string).stringByAppendingString("_")
+        //В textField есть неприятное поведение, когда он при печати резервирует один символ и проскролировает
+        //вперед при подходе к границе. Хотя текст и так бы поместился. Если Вам не нужно такое поведение
+        //То замените строчку выше этой
+        //let newText = text.stringByReplacingCharactersInRange(range, withString: string)
+        
+        //Получаем размеры текста
+        let width = newText.sizeWithAttributes(textField.defaultTextAttributes).width
+        //Получаем размеры области ввода
+        let widthOfEditingRect = textField.editingRectForBounds(textField.bounds).width
+        
+        //Разрешаем ввод только если текст не больше области ввода
+        return (width  <= widthOfEditingRect);
+    }
 }
 
